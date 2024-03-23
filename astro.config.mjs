98f6ad1f3f63910b28f1623f85d4d67a608b2351
OG'ABEK <o.shomurotov@uic.group>
2024-03-23 09:41:24 +0500

    fix: update package.json & add biome.json format rules

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
 
 import vue from "@astrojs/vue";
 import react from "@astrojs/react";
@@ -7,8 +8,8 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), react(), svelte()],
+  integrations: [tailwind(), astroI18next(), vue(), react(), svelte()],
   server: {
-    host: true
+    port: 3000,
   }
 });
