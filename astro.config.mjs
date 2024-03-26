4a85243ae24aba1c9352cb0d5323d7548f3a4cf4
OG'ABEK <o.shomurotov@uic.group>
2024-03-26 23:53:25 +0500

    feat: vue-sonner added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
-
 import vue from "@astrojs/vue";
-import react from "@astrojs/react";
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), vue(), react(), svelte()],
+  integrations: [tailwind(), astroI18next(), vue()],
   server: {
-    port: 3000,
+    port: 3000
   }
 });
--- src/components/vue/InputMask.vue
+++ src/components/vue/InputMask.vue
@@ -1,7 +1,12 @@
 <script setup>
 import { vMaska } from "maska"
+import {toast} from "vue-sonner";
 </script>
 
 <template>
-  <input v-maska data-maska="#-#">
+  <div>
+    <input v-maska data-maska="#-#">
+    <hr>
+    <button @click="toast('Hello!')">Show Toast</button>
+  </div>
 </template>
